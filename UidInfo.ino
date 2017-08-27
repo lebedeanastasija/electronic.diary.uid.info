ef23733001da9c3c72d8e5f18b61548212fd18ad
nastya <lebedeanastasija@yandex.by>
2017-08-27 14:42:10 +0300

    Initial commit

---
+++ UidInfo.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+constexpr uint8_t RST_PIN = 9;
+constexpr uint8_t SS_PIN = 10;
+
+String inStringDec = "";
+String inStringHexRev = "";
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+}
+
+void loop() {
+	// Look for new cards
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+  
+  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
+  // I think we should assume every PICC as they have 4 byte UID
+  // Until we support 7 byte PICCs
+  String inStringHex = "";
+  for ( byte i = 0; i < mfrc522.uid.size; i++) {
+     inStringHex += String(mfrc522.uid.uidByte[i], HEX);
+  }
+  
+  long unsigned uid = strtol(inStringHex.c_str(),NULL,16);
+  Serial.println(uid);
+  mfrc522.PICC_HaltA(); // Stop reading
+}
